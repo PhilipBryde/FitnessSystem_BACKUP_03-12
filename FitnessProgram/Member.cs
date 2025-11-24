c30a51198690238accc043210957d194cf013a7f
cpu-man <71815606+cpu-man@users.noreply.github.com>
2025-11-24 10:36:06 +0100

    NewStuff

--- FitnessProgram/Member.cs
+++ FitnessProgram/Member.cs
@@ -8,13 +8,14 @@ namespace FitnessProgram
 {
     public class Member
     {
-        //Opretter forskellige variabler til vores medlemmer;de er alle tomme og kan få en værdi senere hen når vi får brug for det (ved brug af get; set,)
+        public List<Member> memberList = new List<Member>(); //liste hvor de forskellige medlemmer bliver tilføjet ind i
+        //Opretter forskellige variabler til vores medlemmer;de er alle tomme og kan tilgåes og ændres senere hen (ved brug af get; set,)
         public int id { get; set; }
         public string name { get; set; }
         public char gender { get; set; }
         //public string password;
 
-        public Member(int id, string name, char gender)
+        public Member(int id, string name, char gender) //Constructor som gør det muligt at give vores medlem's variabler nye værdier hver gang vi laver et nyt objekt med dem
         {
             this.id = id;
             this.name = name;
--- FitnessProgram/NextPage.xaml.cs
+++ FitnessProgram/NextPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,18 +16,13 @@ using System.Windows.Shapes;
 namespace FitnessProgram
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NextPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NextPage : Page
     {
-        public MainWindow()
+        public NextPage()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }
